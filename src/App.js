1f89834221269f852e6faf3cb0ebea64376ef4b2
SereenQamhia <Sereen.qamhia@gmail.com>
2023-10-03 20:33:39 +0300

    Sereen

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import About from './pages/about';
+import './App.css';
+
+function App() {
+  return (
+    <div >
+     <About />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/about.js
@@ -0,0 +1,45 @@
+import React from "react";
+
+function About() {
+  return (
+    <div >
+        
+    <div class="card-body p-0">
+                                        <div class="px-2 px-md-3 py-2 py-md-1 border-bottom border-color-8">
+                                            <div class="media p-2 p-md-3">
+                                                <div class="u-avatar u-lg-avatar-md mr-2 mr-md-3">
+                                                    
+                                                </div>
+                                                <div class="media-body position-relative pl-md-1">
+                                                    <div class="d-flex justify-content-between align-items-start mb-2 mb-md-3">
+                                                        <span class="d-block text-dark font-weight-bold">Hyatt Centric Times  Square</span>
+                                                        <button type="button" class="close close-rounded position-md-absolute right-0 ml-2" aria-label="Close">
+                                                            <i class="fas fa-trash"></i>
+                                                        </button>
+                                                    </div>
+                                                    <span class="d-block text-gray-1">Price  £590.00 </span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="px-2 px-md-3 py-2 py-md-1 border-bottom border-color-8">
+                                            <div class="media p-2 p-md-3">
+                                                <div class="u-avatar u-lg-avatar-md mr-2 mr-md-3">
+                                                  
+                                                </div>
+                                                <div class="media-body position-relative pl-md-1">
+                                                    <div class="d-flex justify-content-between align-items-start mb-2 mb-md-3">
+                                                        <span class="d-block text-dark font-weight-bold">Hyatt Centric Times  Square</span>
+                                                        <button type="button" class="close close-rounded position-md-absolute right-0 ml-2" aria-label="Close">
+                                                            <i class="fas fa-trash"></i>
+                                                        </button>
+                                                    </div>
+                                                    <span class="d-block text-gray-1">Price  £590.00 </span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>                   
+    </div>
+  );
+}
+
+export default About;
