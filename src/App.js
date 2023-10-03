4d14e2a72de92ad280138d9e177d4626ce0c4e45
shatharababah22 <shatha.rababah22@gmail.com>
2023-10-03 23:17:57 +0300

    shatha

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import logo from './logo.svg';
-import About from './pages/about';
-import './App.css';
+import React from "react";
+
+// import Home from "./pages/home/home"; 
+import Header from "./pages/home/header";
+import "./App.css";
 
 function App() {
   return (
-    <div >
-     <About />
-     <h1>hay</h1>
+    <div>
+     
+      {/* <Home />  */}
+      <Header />
     </div>
   );
 }
