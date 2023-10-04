4affa03cf2a46ed1eccb5b50458a632efbf5910f
ahmadkhatatbe <ahmedkatatbeh123@gmail.comgit config --global user.email ahmedkatatbeh123@gmail.com>
2023-10-04 15:10:45 +0300

    Co-authored-by: Yousef-Jaradat <135589126+Yousef-Jaradat@users.noreply.github.com> Co-authored-by: Yousef-M-Jaradat <135706124+Yousef-M-Jaradat@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-// import logo from './logo.svg';
-// import About from './pages/about';
-// import Booking from './pages/Booking';
+
 import './App.css';
 import React from "react";
-// import Home from "./pages/home/home"; 
-
 import Navbar from "./pages/home/navbar";
+import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Booking from "./pages/booking";
+import Header from "./pages/home/navbar";
 
 import "./style.css";
 
 
 function App() {
   return (
- 
-    <div> 
-      
-    
-   <Navbar/>
-   
-    
+    <div>
+      <Navbar />
+      <Routes>
+      <Route path="/Register" element={<Register />} />
+      <Route path="/Login" element={<Login />} />
+      <Route path="/booking" element={<Booking />} />
+      </Routes>
     </div>
   );
 }
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,82 +1,94 @@
+import React, { useState } from 'react';
+import axios from "axios";
+import { Navigate, useNavigate } from "react-router-dom"; // Import useNavigate
 
-import React from 'react'
-import { useState } from 'react';
- function Register() {
- const [username, setusername] = useState();
-  const [email, setemail] = useState();
-   const [password, setpassword] = useState();
+function Register() {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const apiUrl = "https://64db17df593f57e435b06a91.mockapi.io/AHMED";
+  const navigate = useNavigate(); // Initialize useNavigate for navigation
 
-   const handle_email = (e)=>{
-     setemail(e.target.value);
-   }
-   const handle_password = (e) => {
-     setpassword(e.target.value);
+  const handleUsername = (e) => {
+    setUsername(e.target.value);
+  };
 
-   };
-   const handle_username = (e) => {
-     setusername(e.target.value);
+  const handleEmail = (e) => {
+    setEmail(e.target.value);
+  };
 
-   };
-   const submit = (e) => {
-    e.preventDefault();
-     console.log(username)
-          console.log(email);
+  const handlePassword = (e) => {
+    setPassword(e.target.value);
+  };
 
-               console.log(password);
+  const handleSubmit = (e) => {
+    e.preventDefault(); // Prevent the form from submitting the default way
 
-   };
+    // Create an object to send in the request body
+    const userData = {
+      username: username,
+      email: email,
+      password: password,
+    };
 
+    axios
+      .post(apiUrl, userData)
+      .then((response) => {
+        const responseData = response.data;
+        // Handle the successful response here
+      })
+      .catch((error) => {
+        // Handle any errors that occurred during the request
+        console.error("Error:", error);
+      });
+    navigate("/Login");
+  };
 
-   return (
-     <div>
-         <div id="login-form-wrap">
-         <h2>Register</h2>
-         <form onClick={submit} id="login-form">
-           <p>
-             <input
-               onChange={handle_username}
-                 
-               type="text"
-               id="username"
-               name="username"
-               placeholder="Username"
-               required
-             />
-             
-           </p>
-           <p>
-             <input
-               onChange={handle_email}
-               type="email"
-               id="email"
-               name="email"
-               placeholder="Email Address"
-               required
-             />
-             
-           </p>
-           <p>
-             <input
-               onChange={handle_password}
-               type="text"
-               id="password"
-               name="password"
-               placeholder="Password"
-               required
-             />
-             
-           </p>
-           
-           <p>
-             <input type="submit" id="login" value="Register" />
-           </p>
-         </form>
-         <div id="create-account-wrap">
-           <p>{/* Not a member? <a href="#">Create Account</a> */}</p>
-         </div>
-       </div>
-      
-     </div>
-   );
+  return (
+    <div>
+      <div id="login-form-wrap">
+        <h2>Register</h2>
+        <form onSubmit={handleSubmit} id="login-form">
+          <p>
+            <input
+              onChange={handleUsername}
+              type="text"
+              id="username"
+              name="username"
+              placeholder="Username"
+              required
+            />
+          </p>
+          <p>
+            <input
+              onChange={handleEmail}
+              type="email"
+              id="email"
+              name="email"
+              placeholder="Email Address"
+              required
+            />
+          </p>
+          <p>
+            <input
+              onChange={handlePassword}
+              type="password"
+              id="password"
+              name="password"
+              placeholder="Password"
+              required
+            />
+          </p>
+          <p>
+            <input type="submit" id="login" value="Register" />
+          </p>
+        </form>
+        <div id="create-account-wrap">
+          <p>{/* Not a member? <a href="#">Create Account</a> */}</p>
+        </div>
+      </div>
+    </div>
+  );
 }
+
 export default Register;
